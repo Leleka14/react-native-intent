57bb97999b1eff075dd8f615c3a0f9cfb0b0de82
Oleh Deneka <docodestudio@docodes-MacBook-Pro.local>
2021-06-22 16:32:31 +0300

    saving progress after fixing bugs

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    semi: false,
+    printWidth: 80,
+    'react/jsx-sort-props': [
+      true,
+      {
+        shorthandFirst: true,
+  
+        ignoreCase: true,
+      },
+    ],
+  }
+  
